62a8f9675a5e7cca3d5f437caa77f2acde80f1fa
inoi2000 <nikolaiklonov@yandex.ru>
2023-05-14 13:43:09 +0500

    Добавьте файлы проекта.

---
+++ DownloadUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using NetworlPrograming;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Runtime.InteropServices;
+
+namespace DownloadUI.ViewModel
+{
+    public partial class MainViewModel : INotifyPropertyChanged
+    {
+        private static CancellationTokenSource cts = new CancellationTokenSource();
+
+        #region Properties
+        private double _progress;
+        public double Progress
+        {
+            get { return _progress; }
+            set { _progress = value; OnPropertyChanged(); }
+        }
+
+        private string _uri;
+        public string Uri
+        {
+            get { return _uri; }
+            set { _uri = value; OnPropertyChanged(); }
+        }
+
+        private string _path;
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region Commands
+        [RelayCommand]
+        public async Task DownloadingFile()
+        {
+            var progress = new Progress<double>();
+            progress.ProgressChanged += (sender, current) =>
+            {
+                Progress = current;
+            };
+
+            CancellationToken token = cts.Token;
+
+            await FileDowndoad.DownloadingFileAsync(Uri, Path, progress, token);
+        }
+
+        [RelayCommand]
+        public void CancelDownloading()
+        {
+            try
+            {
+                cts.Cancel();
+            }
+            catch { }
+        }
+        #endregion
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ NetworlPrograming/FileDowndoad.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworlPrograming
+{
+    public static class FileDowndoad
+    {
+        public static async Task DownloadingFileAsync(string uri, string outputPath, IProgress<double> progress, CancellationToken token)
+        {
+            if (string.IsNullOrEmpty(uri)) 
+            { 
+                throw new ArgumentException($"\"{nameof(uri)}\" не может быть неопределенным или пустым.", nameof(uri)); 
+            }
+
+            if (string.IsNullOrEmpty(outputPath))
+            {
+                throw new ArgumentException($"\"{nameof(outputPath)}\" не может быть неопределенным или пустым.", nameof(outputPath));
+            }
+
+            long localFileLen = 0;
+            FileStream file;
+            if (File.Exists(outputPath))
+            {
+                localFileLen = new FileInfo(outputPath).Length;
+                file = File.OpenWrite(outputPath);
+                file.Seek(0, SeekOrigin.End);
+            }
+            else
+            {
+                file = File.Create(outputPath);
+            }
+
+            using var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, uri)
+            {
+                Headers = { Range = new RangeHeaderValue(localFileLen, null) }
+            };
+            
+            using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+
+            long? contentLength = response.Content.Headers.ContentLength; 
+            long totalBytesRead = 0;
+
+            response.EnsureSuccessStatusCode();
+
+            long? contenerLength = response.Content.Headers.ContentLength;
+
+            await using var contentStream = await response.Content.ReadAsStreamAsync();
+
+            byte[] buffer = new byte[8192 * 2];
+
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(10)); // намеренное замедление для проверки функционала прогресса и отмены
+
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+
+                int bytesRead = await contentStream.ReadAsync(buffer);
+
+                if (bytesRead == 0) { break; }
+
+                if(bytesRead == buffer.Length) { await file.WriteAsync(buffer, token); }
+                else { await file.WriteAsync(buffer[..bytesRead], token); }
+
+                totalBytesRead += bytesRead;
+                if (contentLength != null)
+                {
+                    progress.Report((double)totalBytesRead / contentLength.Value);
+                }
+            }
+            file.Close();
+        }
+    }
+}
